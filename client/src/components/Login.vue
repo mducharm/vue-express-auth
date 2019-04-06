fd79940ecf7d9e676faebdeb3f0741d1d3c0618a
Michael <sheveksmath@gmail.com>
2019-04-06 17:21:10 -0400

    added vue router, set up login component

---
+++ client/src/components/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit="login">
+      <input type="text" name="email">
+      <br>
+      <input type="password" name="password">
+      <br>
+      <input type="submit" value="Login">
+    </form>
+  </div>
+</template>
+
+<script>
+import router from "../router";
+import axios from "axios";
+export default {
+  name: "Login",
+  methods: {
+    login: e => {
+      let email = "user@email.com";
+      let password = "password";
+      let login = () => {
+        let data = {
+          email: email,
+          password: password
+        };
+        axios
+          .post("/api/login", data)
+          .then(response => {
+            console.log("Logged in");
+            router.push("/dashboard");
+          })
+          .catch(errors => {
+            console.log("Cannot log in");
+          });
+      };
+      login();
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
